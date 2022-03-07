a20362dd8ea79779a31a7d2efb36567a713e39d2
Stefan Holst <stefan.holst@keysight.com>
2022-03-07 21:21:43 +0100

    Updated install to work on windows by setting os specific paths

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const tc = require('@actions/tool-cache');
+import { EOL } from 'os';
 
 const WIN_INSTALL_PATH = "C:/Program Files/OpenTAP";
 const UNIX_INSTALL_PATH = "/opt/tap";
@@ -9,7 +10,7 @@ main().catch((error) => setFailed(error.message));
 
 async function main() {
   try {
-    const isUnix = tc.IS_WINDOWS == false;
+    const isUnix = process.platform !== "win32";
     const destDir = isUnix ? UNIX_INSTALL_PATH : WIN_INSTALL_PATH;
 
     let args = [];
