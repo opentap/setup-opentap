ce0425a67a5fdb36b2fc7e5705d3e692df75ca52
Stefan Holst <stefan.holst@keysight.com>
2022-03-07 16:57:39 +0100

    Updates

--- index.js
+++ index.js
@@ -5,52 +5,51 @@ import { promisify } from "util";
 const writeFileAsync = promisify(writeFile);
 
 try {
-    let args = [];
-
     // Get version of opentap to download
     var opentapVersion = core.getInput('version');
     if (opentapVersion){
+      let args = [];
       args.push("version=" + opentapVersion);
+
+      // Get current arch
+      args.push("architecture=x64")
+
+      // Get current os
+      args.push("os=linux")
+      
+      // Download OpenTAP
+      core.info('Installing OpenTAP: ' + args);
+      const downloadedFilepath = await tc.downloadTool('https://packages.opentap.io/3.0/DownloadPackage/OpenTAP?' + args.join("&"));
+
+      // Extract OpenTAP package
+      await tc.extractZip(downloadedFilepath, 'opt/tap');
+
+      // Add to path env
+      core.addPath('/opt/tap')
     }
-
-    // Get current arch
-    args.push("architecture=x64")
-
-    // Get current os
-    args.push("os=linux")
-    
-    // Download OpenTAP
-    core.info('Installing OpenTAP: ' + args);
-    const downloadedFilepath = await tc.downloadTool('https://packages.opentap.io/3.0/DownloadPackage/OpenTAP?' + args.join("&"));
-
-    // Extract OpenTAP package
-    await tc.extractZip(downloadedFilepath, 'opt/tap');
-
-    // Add to path env
-    core.addPath('/opt/tap')
 
     // Install packages
     var packages = core.getInput('packages')
     if (packages){
-        core.info('Installing packages: ' + packages);
+      core.info('Installing packages: ' + packages);
 
-        var image = {
-          Packages: packages.map(p => {
-            Name: p.name
-            Version: p.version
-          }),
-          Repositories: ["https://packages.opentap.io"]
-        }
+      var image = {
+        Packages: packages.map(p => {
+          Name: p.name
+          Version: p.version
+        }),
+        Repositories: ["https://packages.opentap.io"]
+      }
 
-        writeFileAsync("image.json", JSON.stringify(image))
+      writeFileAsync("image.json", JSON.stringify(image))
 
-        exitCode = exec.exec('tap', ["image",  "install", "image.json"], {
-            listeners: {
-              stdout: data => {
-                core.info(data.toString())
-              }
+      exitCode = exec.exec('tap', ["image",  "install", "image.json"], {
+          listeners: {
+            stdout: data => {
+              core.info(data.toString())
             }
-        });
+          }
+      });
     }
 } 
 catch (error) {
