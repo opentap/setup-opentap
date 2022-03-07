624c103811799585b9463c24b635324cf4a9b0e8
Stefan Holst <stefan.holst@keysight.com>
2022-03-07 17:10:59 +0100

    Updates

--- index.js
+++ index.js
@@ -2,43 +2,48 @@ const core = require('@actions/core');
 const { exec } = require('@actions/exec');
 const tc = require('@actions/tool-cache');
 
-try {
-    // Get version of opentap to download
-    var opentapVersion = core.getInput('version');
-    if (opentapVersion){
-      let args = [];
-      args.push("version=" + opentapVersion);
 
-      // Get current arch
-      args.push("architecture=x64")
+main().catch((error) => setFailed(error.message));
 
-      // Get current os
-      args.push("os=linux")
-      
-      // Download OpenTAP
-      core.info('Installing OpenTAP: ' + args);
-      const downloadedFilepath = await tc.downloadTool('https://packages.opentap.io/3.0/DownloadPackage/OpenTAP?' + args.join("&"));
+async function main() {
+  try {
+      // Get version of opentap to download
+      var opentapVersion = core.getInput('version');
+      if (opentapVersion){
+        let args = [];
+        args.push("version=" + opentapVersion);
 
-      // Extract OpenTAP package
-      await tc.extractZip(downloadedFilepath, 'opt/tap');
+        // Get current arch
+        args.push("architecture=x64")
 
-      // Add to path env
-      core.addPath('/opt/tap')
-    }
+        // Get current os
+        args.push("os=linux")
+        
+        // Download OpenTAP
+        core.info('Installing OpenTAP: ' + args);
+        const downloadedFilepath = await tc.downloadTool('https://packages.opentap.io/3.0/DownloadPackage/OpenTAP?' + args.join("&"));
 
-    // Install packages
-    var package = core.getInput('package')
-    if (package){
-      core.info('Installing package: ' + package);
-      exitCode = exec.exec('tap', ["package",  "install", package], {
-          listeners: {
-            stdout: data => {
-              core.info(data.toString())
+        // Extract OpenTAP package
+        await tc.extractZip(downloadedFilepath, 'opt/tap');
+
+        // Add to path env
+        core.addPath('/opt/tap')
+      }
+
+      // Install packages
+      var package = core.getInput('package')
+      if (package){
+        core.info('Installing package: ' + package);
+        exitCode = exec.exec('tap', ["package",  "install", package], {
+            listeners: {
+              stdout: data => {
+                core.info(data.toString())
+              }
             }
-          }
-      });
-    }
-} 
-catch (error) {
-  core.setFailed(error.message);
+        });
+      }
+  } 
+  catch (error) {
+    core.setFailed(error.message);
+  }
 }
